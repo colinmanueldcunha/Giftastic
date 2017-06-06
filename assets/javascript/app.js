bc3df1308819fda48085179156d775ee8e600ef4
colinmanueldcunha <colinmdcunha@gmail.com>
2017-06-06 05:54:37 -0400

    gifappjs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,148 +1,144 @@
-var intervalId;
-var trivia = {
-    time: 30,
-    correct: 0,
-    incorrect: 0,
-    unanswered: 0,
-    writeq: 1,
-    questions: {
-        q1: {
-            q: "What was the most selling game in history?",
-            a: "Wii Sports",
-            c: ["Wii Sports", "Tetris", "Pong", "Super Mario Bros."],
-            img: "assets/images/wiiSports.png"
-        },
-        q2: {
-            q: "In what year was Sega Genesis released in North America?",
-            a: "1989",
-            c: ["1989", "1999", "1991", "1975"],
-            img: "assets/images/segaGen.png"
-        },
-        q3: {
-            q: "Which of the following video games takes place in a dystopian underwater city called Rapture?",
-            a: "Bioshock",
-            c: ["Bioshock", "Fallout 3", "God of War", "Half-Life"],
-            img: "assets/images/bioshock.png"
-        },
-        q4: {
-            q: "E. Honda, Dhalsim and Chun Li are all characters from what video game series?",
-            a: "Street Fighter",
-            c: ["Street Fighter", "Tekken", "Battletoads", "Mortal Kombat"],
-            img: "assets/images/ryu.png"
-        },
-        q5: {
-            q: "What classic beat-'em-up game featured brothers Billy Lee and Jimmy (also nicknamed Spike and Hammer)?",
-            a: "Double Dragon",
-            c: ["Double Dragon", "Smash Brothers", "Snow Brothers", "Ninja Gaiden"],
-            img: "assets/images/doubleDrag.png"
-        },
-        q6: {
-            q: "How many bits was the Super Nintendo Entertainment System?",
-            a: "16",
-            c: ["16", "8", "128", "64"],
-            img: "assets/images/snes.png"
-        },
-        q7: {
-            q: "What character do you play as in The Legend Of Zelda?",
-            a: "Link",
-            c: ["Link", "Zelda", "Gandolf", "Tingle"],
-            img: "assets/images/link.png"
-        }
+$(document).ready(function(){
 
-    },
-    start: function() {
-        // debugger;
-        trivia.writeToDiv(trivia.writeq);
-        intervalId = setInterval(trivia.count, 1000);
-    },
-    stop: function() {
-        clearInterval(intervalId);
-    },
-    count: function() {
-        trivia.time--;
-        $("#time").html("Time left: " + trivia.time);
-        if (trivia.time === 0) {
-            trivia.stop();
-            trivia.unanswered++;
-            trivia.buffer();
-        }
-    },
-    oblength: function() {
-        return (Object.keys(trivia.questions).length);
-    },
-    writeToDiv: function(i) {
-        // debugger;
-        var questions = $("<div>");
-        questions.addClass("hideThis");
-        var theQuestion = trivia.questions["q" + i].q;
-        var pone = $("<p id='questions'>").text(theQuestion);
-        questions.append(pone);
-        var randomChoice = trivia.questions["q" + i].c.sort(function() {
-            return 0.5 - Math.random();
-        });
-        for (var n = 0; n < randomChoice.length; n++) {
-            var choice = randomChoice[n];
-            var choices = $("<button>").text(choice);
-            choices.addClass("options");
-            choices.attr("data-name", randomChoice[n]);
-            questions.append(choices);
-            questions.append("<br>")
-        }
-        $("#mainContent").html(questions);
-    },
-    scorer: function() {
-        var userguess = $(this).data("name");
-        trivia.stop();
-        if (userguess == trivia.questions["q" + trivia.writeq].a) {
-            console.log("YEE");
-            trivia.correct++;
-            trivia.buffer();
-        } else {
-            console.log("NEE");
-            trivia.incorrect++;
-            trivia.buffer();
-        }
-    },
-    buffer: function() {
-        trivia.time = 30;
-        $("#time").html("Time left: " + trivia.time);
-        var imgname = trivia.questions["q"+trivia.writeq].img;
-        $("#mainContent").html("<br><p>Answer: " + trivia.questions["q" + trivia.writeq].a + "</p><p>Correct: " + trivia.correct + "</p><p>Incorrect: " + trivia.incorrect + "</p><p>Unanswered: " + trivia.unanswered + "</p><br><img id='previewpic' src="+imgname+">");
-        trivia.writeq++;
-        if (trivia.writeq <= (trivia.oblength())) {
-            setTimeout(trivia.start, 1000 * 3);
-        } else {
-            setTimeout(trivia.endgame, 1000 *3);
-        }
-    },
-    endgame: function() {
-        $("#title").html("<img id=endgamepic src='assets/images/thanks.jpg'>");
-        $("#time").empty();
-        $("#mainContent").html("<br><p>Total Scores!</p><p>Correct: " + trivia.correct + "</p><p>Incorrect: " + trivia.incorrect + "</p><p>Unanswered: " + trivia.unanswered + "</p>");
-    }
-};
-window.onload = function() {
-    // debugger;
-    $("#start").click(function() {
-        $("#start").hide();
-        $("#time").html("Time left: " + trivia.time);
-        trivia.start();
+    $('button').on('click', function() {
+        var animal = $(this).data('name');
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        })
+            .done(function(response) {
+
+
+                console.log(response)
+
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+
+                    var animalDiv = $('<div/>');
+
+                    var p =$('<p/>');
+
+                    p.text(results[i].rating);
+
+                    var animalImage = $('<img/>');
+
+                    animalImage.addClass('anImg')
+
+                    animalImage.attr('src', results[i].images.fixed_height.url);
+
+                    animalImage.attr('data-still', results[i].images.fixed_height_still.url)
+
+                    animalImage.attr('data-animate', results[i].images.fixed_height.url)
+
+                    .attr('data-state', 'still');
+
+                    animalDiv.append(p);
+
+                    animalDiv.append(animalImage);
+
+                    animalDiv.prependTo($('#gifs'));
+                }
+
+                $('.anImg').on('click', function() {
+            
+                    var state = $(this).attr('data-state'); 
+                    console.log(this);
+
+                    if (state == 'still') {
+                    
+                    $(this).attr('src', $(this).data('animate'));
+                    
+                    $(this).attr('data-state', 'animate');
+
+                    } else {
+                            
+                    $(this).attr('src', $(this).data('still'));
+                    
+                    $(this).attr('data-state', 'still');
+                    }      
+                });
+            });
     });
-    $("#mainContent").on("click", ".options", trivia.scorer);
-};
 
+    var animals = [''];
 
+    
+        //This function "adds" the buttons 
 
+        // handles the event when clicked
+        $('#theButton').on('click', function(){
+            var animalButton = $("#gif-input").val();
+            //adds the new animal
 
+            var newButton = $("<button/>").addClass( "btn btn-info animal").attr('data-name',animalButton).html(animalButton).css({'margin': '5px'});
+            
+            $("#animalsbuttons").append(newButton);
+                console.log("Work");
 
+            queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animalButton + "&api_key=dc6zaTOxFJmzC&limit=10";
+                console.log(animalButton);
 
+            $.ajax({
+            url: queryURL,
+            method: 'GET'
+            })
 
+            .done(function(response) {
 
-// function myfunc(){
-// 	console.log("hi!");
-// 	if(true){
-// 		setTimeout(myfunc,2000)
-// 	}
-// }
+            var results = response.data;
 
-// myfunc();
+                for (var i = 0; i < results.length; i++) {
+
+                    var animalDiv = $('<div/>');
+
+                    var p =$('<p/>');
+
+                    p.text(results[i].rating);
+
+                    var animalImage = $('<img/>');
+
+                    animalImage.addClass('anImg')
+
+                    animalImage.attr('src', results[i].images.fixed_height_still.url);
+
+                    animalImage.attr('data-still', results[i].images.fixed_height_still.url)
+
+                    animalImage.attr('data-animate', results[i].images.fixed_height.url)
+
+                    .attr('data-state', 'still');
+
+                    animalDiv.append(p);
+
+                    animalDiv.append(animalImage);
+
+                    animalDiv.prependTo($('#gifs'));
+                }
+
+                $('.anImg').on('click', function() {
+            
+                    var state = $(this).attr('data-state'); 
+                    console.log(this);
+
+                    if (state == 'still') {
+                    
+                    $(this).attr('src', $(this).data('animate'));
+                    
+                    $(this).attr('data-state', 'animate');
+
+                    } else {
+                            
+                    $(this).attr('src', $(this).data('still'));
+                    
+                    $(this).attr('data-state', 'still');
+                    }      
+                });
+            });
+
+            $("#gif-input").val("");
+            return false;
+        })
+  
+});
